92915625d5c09b2eaf72ae06a17c63ffdfb57a54
Avraham Jacobsohn <noreallyimfine@users.noreply.github.com>
2020-03-23 16:19:34 -0400

    implement at least some of constructor class as well as ram_read and ram_write methods

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -1,6 +1,6 @@
 """CPU functionality.
 
-Constructor method not filled out. 
+Constructor method not filled out.
 Load method filled out and straight forward from Monday class
 Alu method ADD completed, needs the rest of the mathematical operations
 Trace method filled out so print out cpu state, useful for debugging
@@ -15,7 +15,9 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = [0] * 8
+        self.register = [0] * 8
+        self.pc = 0
 
     def load(self):
         """Load a program into memory."""
@@ -26,18 +28,23 @@ class CPU:
 
         program = [
             # From print8.ls8
-            0b10000010, # LDI R0,8
+            0b10000010,  # LDI R0,8
             0b00000000,
             0b00001000,
-            0b01000111, # PRN R0
+            0b01000111,  # PRN R0
             0b00000000,
-            0b00000001, # HLT
+            0b00000001,  # HLT
         ]
 
         for instruction in program:
             self.ram[address] = instruction
             address += 1
 
+    def ram_read(self, index):
+        return self.ram[index]
+
+    def ram_write(self, index, value):
+        self.ram[index] = value
 
     def alu(self, op, reg_a, reg_b):
         """ALU operations."""
